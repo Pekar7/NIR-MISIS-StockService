226c570005c3810fdace2a7a0323ad3edce0510e
mac <m1901321@edu.misis.ru>
2022-10-14 17:48:59 +0300

    Refactor Service

--- src/main/java/com/example/serviceStock/service/TinkoffStockService.java
+++ src/main/java/com/example/serviceStock/service/TinkoffStockService.java
@@ -3,15 +3,12 @@ package com.example.serviceStock.service;
 
 import com.example.serviceStock.exception.StockNotFoundException;
 import com.example.serviceStock.model.Currency;
+import com.example.serviceStock.model.PriceInfo;
 import com.example.serviceStock.model.Stock;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import ru.tinkoff.invest.openapi.OpenApi;
-import ru.tinkoff.invest.openapi.model.rest.MarketInstrumentList;
-
-import java.util.concurrent.CompletableFuture;
 
 @Slf4j
 @Service
@@ -19,27 +16,42 @@ import java.util.concurrent.CompletableFuture;
 public class TinkoffStockService implements StockService {
     private final OpenApi api;
 
-    @Async
-    public CompletableFuture<MarketInstrumentList> getMarketInstrumentTicker(String ticker) {
-        log.info("Getting {} from Tinkoff", ticker);
-        var context = api.getMarketContext();
-        return context.searchMarketInstrumentsByTicker(ticker);
-    }
 
     @Override
     public Stock getStockByTicker(String ticker) {
-        var cf = getMarketInstrumentTicker(ticker);
-        var list = cf.join().getInstruments();
-        if (list.isEmpty()) {
+        var contextApi = api.getMarketContext().searchMarketInstrumentsByTicker(ticker);
+        var listContext = contextApi.join().getInstruments();
+        var item = listContext.get(0);
+        String figi = listContext.get(0).getFigi();
+
+        if (listContext.isEmpty()) {
             throw new StockNotFoundException(String.format("Stock %S not found.", ticker));
         }
-        var item = list.get(0);
+
+        var apCon = api.getMarketContext().getMarketOrderbook(figi, 0);
+
         return new Stock(
                 item.getTicker(),
                 item.getFigi(),
+                item.getLot(),
                 item.getName(),
                 item.getType().getValue(),
                 Currency.valueOf(item.getCurrency().getValue()),
+                apCon.join().get().getLastPrice(),
                 "TINKOFF");
     }
+
+
+    @Override
+    public PriceInfo getPriceByFigi(String figi) {
+        var context = api.getMarketContext().getMarketOrderbook(figi, 0);
+        var list = context.join().get();
+        return new PriceInfo(
+                list.getLastPrice(),
+                list.getDepth(),
+                list.getLimitUp(),
+                list.getLimitDown(),
+                list.getTradeStatus());
+    }
+
 }
